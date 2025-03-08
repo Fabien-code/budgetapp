a66a63afb4b0852654d11c52eecb02ebfe24993c
Fabien-Code <fabiengg05@gmail.com>
2025-03-08 12:05:06 +0100

    edit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Budget App", layout="wide")
 st.title("🏠 Bienvenue sur l'application Budget")
 
 st.sidebar.success("Sélectionnez une page ci-dessus.")
-firebase_key = os.getenv('FIREBASE_KEY_JSON')
+firebase_key = os.getenv('FIREBASE_KEY')
 
 if not firebase_admin._apps:
     # Initialisation de Firebase avec la clé privée
