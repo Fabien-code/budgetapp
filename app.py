ff360679169a61ae92eb73ceaddd76be67133d94
Fabien-Code <fabiengg05@gmail.com>
2025-03-08 12:03:06 +0100

    key issue

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ st.set_page_config(page_title="Budget App", layout="wide")
 st.title("🏠 Bienvenue sur l'application Budget")
 
 st.sidebar.success("Sélectionnez une page ci-dessus.")
-
+firebase_key = os.getenv('FIREBASE_KEY_JSON')
 
 if not firebase_admin._apps:
     # Initialisation de Firebase avec la clé privée
     
-    cred = credentials.Certificate("firebase_key.json")  # Chemin vers ta clé
+    cred = credentials.Certificate(firebase_key)  # Chemin vers ta clé
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://budgetdb-13a11-default-rtdb.europe-west1.firebasedatabase.app"  # Remplace par ton URL Firebase
     })
