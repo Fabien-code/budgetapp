9b07535f4887eba3795762485acea7cc0a0b1626
Fabien-Code <fabiengg05@gmail.com>
2025-03-08 11:47:52 +0100

    secret key bug edit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
+import json
+import os
 
 st.set_page_config(page_title="Budget App", layout="wide")
 
@@ -11,6 +13,7 @@ st.sidebar.success("Sélectionnez une page ci-dessus.")
 
 if not firebase_admin._apps:
     # Initialisation de Firebase avec la clé privée
+    
     cred = credentials.Certificate("firebase_key.json")  # Chemin vers ta clé
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://budgetdb-13a11-default-rtdb.europe-west1.firebasedatabase.app"  # Remplace par ton URL Firebase
